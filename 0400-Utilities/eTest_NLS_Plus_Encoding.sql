e7edbef10df3abed3b125d4b1498d70b3f86a4be
mherreradsci <marcelo.herrerat@gmail.com>
2025-03-19 00:20:45 -0300

    Fix eTest_NLS_Plus_Encoding.sql for UTF16

--- 0400-Utilities/eTest_NLS_Plus_Encoding.sql
+++ 0400-Utilities/eTest_NLS_Plus_Encoding.sql
@@ -3,6 +3,8 @@
 --$ NLS_LANG=SPANISH_CHILE.WE8MSWIN1252 sqlplus system@centos-ora11g @eTest_NLS_Plus_Encoding.sql < /tmp/pass
 --$ NLS_LANG=SPANISH_CHILE.UTF8 sqlplus system@centos-ora11g @eTest_NLS_Plus_Encoding.sql < /tmp/pass
 
+--$ NLS_LANG=SPANISH_CHILE.UTF8 sqlplus system@docker-19c-pdb1-testing @eTest_NLS_Plus_Encoding.sql
+
 WHENEVER SQLERROR EXIT SQL.SQLCODE
 
 set echo off lines 600 pagesize 9999
@@ -15,8 +17,6 @@ col VALUE format a30 wrap
 
 SELECT * FROM nls_session_parameters where parameter in('NLS_TERRITORY', 'NLS_LANGUAGE','NLS_NUMERIC_CHARACTERS','NLS_DATE_FORMAT','NLS_DATE_LANGUAGE');
 
-select 'ñ' as ch, chr(241) from dual;
-
 
 -- Valida que el separador decimal sea coma (,)
 select to_number('99,90', '990D00') as num from dual;
@@ -27,10 +27,12 @@ select next_day(trunc(sysdate, 'month'), 'lunes') sd from dual;
 -- Valida que el primer día de la semana sea lunes
 select case to_char(next_day(trunc(sysdate, 'month'), 'lunes'), 'd') when '1' then 'Pass: Lúnes es el primer día de la semana' else 'Fail' end as "Test 1er día de la semana" from dual;
  
-col C_ENCODING new_value V_ENCODING
-
+-- chr(241) = ñ
+--select ascii('ñ'), chr(ascii('ñ')) from dual;
 with encoding as (
-    select case when 'ñ' != chr(241) then 'Fail' else 'Pass: Encoding Ok' end as TestEncoding from dual
+    select case when 'ñ' != chr(ascii('ñ')) then 'Fail' else 'Pass: Encoding Ok' end as TestEncoding from dual
 )     
 select  case testencoding when 'Pass: Encoding Ok' then 0 else 1/0 end as force_error  
 from encoding;
+
+
