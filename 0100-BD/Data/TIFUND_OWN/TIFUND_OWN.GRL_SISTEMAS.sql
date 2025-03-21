5612773fc3a04789e293115253b28f335ab63a18
mherreradsci <marcelo.herrerat@gmail.com>
2025-03-20 23:46:00 -0300

    Fix data scripts to reflect the rename of columns

--- 0100-BD/Data/TIFUND_OWN/TIFUND_OWN.GRL_SISTEMAS.sql
+++ 0100-BD/Data/TIFUND_OWN/TIFUND_OWN.GRL_SISTEMAS.sql
@@ -1,8 +1,8 @@
 set define off
 -- Fecha de generación:23-10-2024 01:40:00
-INSERT INTO TIFUND_OWN.GRL_SISTEMAS(ID_SISTEMA, NOMBRE, DESCRIPCION, NIVEL_SISTEMA) VALUES (q'<DETO>',q'<Developer Tools PL/SQL>',q'<Utilidades para desarrolladores de proyectos PL/SQL, por ejemplo: Validador de Buenas Prácticas PL/SQL>',1);
-INSERT INTO TIFUND_OWN.GRL_SISTEMAS(ID_SISTEMA, NOMBRE, DESCRIPCION, NIVEL_SISTEMA) VALUES (q'<FUND>',q'<Fundaciones PL/SQL>',q'<Componentes fundacionales para proyectos PL/SQL>',1);
-INSERT INTO TIFUND_OWN.GRL_SISTEMAS(ID_SISTEMA, NOMBRE, DESCRIPCION, NIVEL_SISTEMA) VALUES (q'<GECO>',q'<Generador de Código PL/SQL>',q'<Generador de Código para proyectos PL/SQL>',1);
-INSERT INTO TIFUND_OWN.GRL_SISTEMAS(ID_SISTEMA, NOMBRE, DESCRIPCION, NIVEL_SISTEMA) VALUES (q'<NN>',q'<NONAME>',q'<Sistemas no catalogados>',0);
-INSERT INTO TIFUND_OWN.GRL_SISTEMAS(ID_SISTEMA, NOMBRE, DESCRIPCION, NIVEL_SISTEMA) VALUES (q'<TI>',q'<Sistemas intenos de TI>',q'<Sistemas internos de TI que sirven para proyectos internos o como componentes de uso transversal>',0);
+INSERT INTO TIFUND_OWN.GRL_SISTEMAS(SIST_CODIGO, NOMBRE, DESCRIPCION, NIVEL_SISTEMA) VALUES (q'<DETO>',q'<Developer Tools PL/SQL>',q'<Utilidades para desarrolladores de proyectos PL/SQL, por ejemplo: Validador de Buenas Prácticas PL/SQL>',1);
+INSERT INTO TIFUND_OWN.GRL_SISTEMAS(SIST_CODIGO, NOMBRE, DESCRIPCION, NIVEL_SISTEMA) VALUES (q'<FUND>',q'<Fundaciones PL/SQL>',q'<Componentes fundacionales para proyectos PL/SQL>',1);
+INSERT INTO TIFUND_OWN.GRL_SISTEMAS(SIST_CODIGO, NOMBRE, DESCRIPCION, NIVEL_SISTEMA) VALUES (q'<GECO>',q'<Generador de Código PL/SQL>',q'<Generador de Código para proyectos PL/SQL>',1);
+INSERT INTO TIFUND_OWN.GRL_SISTEMAS(SIST_CODIGO, NOMBRE, DESCRIPCION, NIVEL_SISTEMA) VALUES (q'<NN>',q'<NONAME>',q'<Sistemas no catalogados>',0);
+INSERT INTO TIFUND_OWN.GRL_SISTEMAS(SIST_CODIGO, NOMBRE, DESCRIPCION, NIVEL_SISTEMA) VALUES (q'<TI>',q'<Sistemas intenos de TI>',q'<Sistemas internos de TI que sirven para proyectos internos o como componentes de uso transversal>',0);
 COMMIT;
